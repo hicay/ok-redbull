6deba4f385553399c6b0d46f1bb205c5f8f5b81a
Salih Caglar Ispirli <caglarispirli@gmail.com>
2016-11-21 16:59:23 +0200

    F

--- mo.py
+++ mo.py
@@ -1,11 +1,4 @@
-import win32api, win32con
-from ctypes import windll, Structure, c_ulong, byref
 import pyautogui, time
-
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 class Phone:
 
